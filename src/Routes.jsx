26a5467c6ae9d2cdfbe18972e65d10456b070deb
Vadasz Laszlo <maxinteger@gmail.com>
2017-06-14 20:26:18 +0100

    add user name to login

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
+  Switch
 } from 'react-router-dom'
 import Home from './page/Home'
 import ExerciseForm from './page/admin/ExerciseForm'
@@ -11,19 +12,23 @@ import SignUp from './page/user/SignUp'
 import Header from './component/nav/Header'
 import ExerciseList from './page/ExerciseList'
 import Exercise from './page/Exercise'
+import Page404 from './page/Page404'
 
 export default (props) =>
   <Router history={{}}>
     <div className="container">
       <Header/>
       <div>
-        <Route path="/" exact component={Home}/>
-        <Route path="/exercise" exact component={ExerciseList}/>
-        <Route path="/exercise/add" component={ExerciseForm}/>
-        <Route path="/exercise/view/:key" component={Exercise}/>
-        <Route path="/exercise/edit/:key" component={ExerciseForm}/>
-        <Route path="/sign-up" component={SignUp}/>
-        <Route path="/sign-in" component={SignIn}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/exercise" exact component={ExerciseList}/>
+          <Route path="/exercise/add" component={ExerciseForm}/>
+          <Route path="/exercise/view/:key" component={Exercise}/>
+          <Route path="/exercise/edit/:key" component={ExerciseForm}/>
+          <Route path="/sign-up" component={SignUp}/>
+          <Route path="/sign-in" component={SignIn}/>
+          <Route component={Page404}/>
+        </Switch>
       </div>
 
       <footer className="footer">
