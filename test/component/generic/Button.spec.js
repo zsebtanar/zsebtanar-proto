4753e030fbdb0558d68ab99fee66ae2056af5bc5
Vadasz Laszlo <maxinteger@gmail.com>
2017-07-12 20:47:00 +0100

    add Button snapshot tests

---
+++ test/component/generic/Button.spec.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from '../../../src/component/general/Button'
+import renderer from 'react-test-renderer'
+
+describe('Button', () => {
+  it('renders correctly the default', () => {
+    const tree = renderer.create(<Button>button text</Button>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly the submit', () => {
+    const tree = renderer.create(<Button submit>button text</Button>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly the primary', () => {
+    const tree = renderer.create(<Button primary>button text</Button>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
