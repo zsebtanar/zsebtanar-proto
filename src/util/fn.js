0e8b9e6ea532256f21ee79e38275db7e73dea224
Vadasz Laszlo <maxinteger@gmail.com>
2017-07-15 12:05:15 +0100

    add assert function

--- src/util/fn.js
+++ src/util/fn.js
@@ -1,3 +1,11 @@
-import { path, pipe, sortBy, toPairs } from 'ramda'
+import { curry, path, pipe, sortBy, toPairs } from 'ramda'
 
 export const pairsInOrder = pipe(toPairs, sortBy(path([1, 'order'])))
+
+export const assert = curry((predicate, message, value) => {
+  if (predicate(value)) {
+    return value
+  } else {
+    throw new Error(message)
+  }
+})
--- test/util/fn.spec.js
+++ test/util/fn.spec.js
@@ -1,15 +1,28 @@
-import { pairsInOrder } from '../../functions/fn'
+import { identity } from 'ramda'
+import { assert, pairsInOrder } from '../../src/util/fn'
 
-test('pairsInOrder an empty object', () => {
-  expect(pairsInOrder({})).toEqual([])
+describe('pairsInOrder', () => {
+  it('an empty object', () => {
+    expect(pairsInOrder({})).toEqual([])
+  })
+
+  it('an not empty object with order property', () => {
+    expect(pairsInOrder({a: {order: 1}, b: {order: 100}, c: {order: 0}}))
+      .toEqual([['c', {order: 0}], ['a', {order: 1}], ['b', {order: 100}]])
+  })
+
+  it('an not empty object without order property', () => {
+    expect(pairsInOrder({a: {}, b: {}, c: {}}))
+      .toEqual([['a', {}], ['b', {}], ['c', {}]])
+  })
 })
 
-test('pairsInOrder an not empty object with order property', () => {
-  expect(pairsInOrder({a: {order: 1}, b: {order: 100}, c: {order: 0}}))
-    .toEqual([['c', {order: 0}], ['a', {order: 1}], ['b', {order: 100}]])
-})
+describe('assert', () => {
+  it('should throw error if the predication is false', () => {
+    expect(() => assert(identity, 'custom error message', false)).toThrow('custom error message')
+  })
 
-test('pairsInOrder an not empty object without order property', () => {
-  expect(pairsInOrder({a: {}, b: {}, c: {}}))
-    .toEqual([['a', {}], ['b', {}], ['c', {}]])
+  it('should not throw error if the predication is true', () => {
+    expect(() => assert(identity, 'custom error message', true)).not.toThrow('custom error message')
+  })
 })
