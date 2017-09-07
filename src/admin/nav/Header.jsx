71fe865f5ef5ec2fbd9ae4f25098eef68bedd78d
Vadasz Laszlo <maxinteger@gmail.com>
2017-09-07 21:10:40 +0100

    zsebtanar-proto-36 Kijelentkezés logó

--- src/admin/nav/Header.jsx
+++ src/admin/nav/Header.jsx
@@ -6,74 +6,96 @@ import { signOut } from '../../shared/store/actions/auth'
 import { openFileManager } from 'shared/store/actions/modal'
 import Button from 'shared/component/general/Button'
 import { openSideNav } from 'shared/store/reducers/sideNav'
+import { Dropdown, DropdownDevider, DropdownMenu, DropdownToggle } from 'shared/ui/Dropdown'
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   session: state.app.session,
   sideNav: state.app.sideNav
 })
 
-export default withRouter(connect(mapStateToProps, {signOut, openFileManager, openSideNav})(function Header (props) {
-  const openFileManager = (e) => {
-    e.preventDefault()
-    props.openFileManager()
-  }
+export default withRouter(
+  connect(mapStateToProps, { signOut, openFileManager, openSideNav })(function Header(props) {
+    const openFileManager = e => {
+      e.preventDefault()
+      props.openFileManager()
+    }
 
-  if (!props.session.signedIn) return <div/>
+    if (!props.session.signedIn) return <div />
 
-  return (
-    <div className="header clearfix">
-      <div className="desktop-header">
-        <nav>
-          <ul className="nav nav-pills float-right">
+    return (
+      <div className="header clearfix">
+        <div className="desktop-header">
+          <nav>
+            <ul className="nav nav-pills float-right">
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Publikus
+                </a>
+              </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="/">Publikus</a>
-            </li>
+              <Dropdown elementType="li" >
+                <DropdownToggle>Rendszer</DropdownToggle>
+                <DropdownMenu>
+                  <NavLink activeClassName="active" className="dropdown-item" to="/user">
+                    <i className="fa fa-users"/> Felhasználók
+                  </NavLink>
+                  <NavLink activeClassName="active" className="dropdown-item" to="/feedback">
+                    <i className="fa fa-commenting-o"/> Visszajelzések
+                  </NavLink>
+                </DropdownMenu>
+              </Dropdown>
 
-            <li className="nav-item">
-              <NavLink activeClassName="active" className="nav-link" to="/user">Felhasználók</NavLink>
-            </li>
+              <Dropdown elementType="li" >
+                <DropdownToggle>Tartalom</DropdownToggle>
+                <DropdownMenu>
+                  <NavLink activeClassName="active" className="dropdown-item" to="/exercise">
+                    <i className="fa fa-tasks"/> Feladatok
+                  </NavLink>
+                  <NavLink activeClassName="active" className="dropdown-item" to="/classification">
+                    <i className="fa fa-folder"/> Kategóriák
+                  </NavLink>
+                  <DropdownDevider/>
+                  <a className="dropdown-item" href="#" onClick={openFileManager}>
+                    <i className="fa fa-picture-o"/> Képek
+                  </a>
+                </DropdownMenu>
+              </Dropdown>
 
-            <li className="nav-item">
-              <NavLink activeClassName="active" className="nav-link" to="/feedback">Visszajelzések</NavLink>
-            </li>
+              <Dropdown elementType="li" right className="user-menu">
+                <DropdownToggle>
+                  <span className="fa-stack fa">
+                    <i className="fa fa-circle fa-stack-2x" />
+                    <i className="fa fa-user fa-stack-1x fa-inverse" />
+                  </span>
+                </DropdownToggle>
+                <DropdownMenu>
+                  <a href="#" className="dropdown-item" onClick={props.signOut}>
+                    <i className="fa fa-power-off" /> Kijelentkezés
+                  </a>
+                </DropdownMenu>
+              </Dropdown>
+            </ul>
+          </nav>
+          <NavLink exact to="/" className="logo-link">
+            <h4 className="text-muted logo" />
+          </NavLink>
+        </div>
 
-            <li className="nav-item">
-              <NavLink activeClassName="active" className="nav-link" to="/classification">Kategóriák</NavLink>
-            </li>
+        <div className="mobile-header ">
+          <Button
+            className="navbar-toggler float-left"
+            onAction={props.openSideNav}
+            aria-expanded={props.sideNav.active}
+            aria-label="Menü megnyitása"
+          >
+            <span className="fa fa-bars fa-lg" />
+          </Button>
 
-            <li className="nav-item">
-              <a className="nav-link" href="#" onClick={openFileManager}>Képek</a>
-            </li>
-
-            <li className="nav-item">
-              <NavLink activeClassName="active" className="nav-link" to="/exercise">Feladatok</NavLink>
-            </li>
-
-            <li className="nav-item" key="sing-out">
-              <a href="#" className="nav-link" onClick={props.signOut} title="Kijelentkezés"><i
-                className="fa fa-power-off"/></a>
-            </li>
-          </ul>
-        </nav>
-        <NavLink exact to="/" className="logo-link">
-          <h4 className="text-muted logo"/>
-        </NavLink>
+          <NavLink exact to="/" className="logo-link float-right">
+            <h4 className="text-muted logo" />
+          </NavLink>
+        </div>
       </div>
-
-      <div className="mobile-header ">
-        <Button
-          className="navbar-toggler float-left"
-          onAction={props.openSideNav}
-          aria-expanded={props.sideNav.active}
-          aria-label="Menü megnyitása">
-          <span className="fa fa-bars fa-lg"/>
-        </Button>
-
-        <NavLink exact to="/" className="logo-link float-right">
-          <h4 className="text-muted logo"/>
-        </NavLink>
-      </div>
-    </div>
-  )
-}))
+    )
+  })
+)
---
+++ src/shared/ui/Dropdown.js
@@ -0,0 +1,80 @@
+import * as React from 'react'
+import { propEq } from 'ramda'
+import { uid } from 'shared/util/uuid'
+
+export class Dropdown extends React.Component {
+  id = `id-dd-${uid()}`
+  state = { open: false }
+
+  getComponent(type) {
+    return this.props.children.filter(propEq('type', type))
+  }
+
+  toggleDropdown = () => this.setState({ open: !this.state.open })
+
+  renderToggle() {
+    const toggle = this.getComponent(DropdownToggle)
+    return React.Children.map(toggle, child =>
+      React.cloneElement(child, {
+        id: this.id,
+        active: this.state.open
+      })
+    )
+  }
+
+  renderMenu() {
+    const menu = this.getComponent(DropdownMenu)
+    return React.Children.map(menu, child =>
+      React.cloneElement(child, {
+        id: this.id,
+        active: this.state.open,
+        className: this.props.right ? 'dropdown-menu-right' : ''
+      })
+    )
+  }
+
+  render() {
+    const Elm = this.props.elementType || 'div'
+    return (
+      <Elm
+        className={`nav-item dropdown ${this.props.className || ''} ${this.state.open
+          ? 'show'
+          : ''}`}
+        onMouseOut={this.toggleDropdown}
+        onMouseOver={this.toggleDropdown}
+      >
+        {this.renderToggle()}
+        {this.renderMenu()}
+      </Elm>
+    )
+  }
+}
+
+export function DropdownToggle(props) {
+  return (
+    <a
+      className={`nav-link dropdown-toggle`}
+      id={props.id}
+      aria-haspopup="true"
+      aria-expanded={props.active}
+    >
+      {props.children}
+    </a>
+  )
+}
+
+export function DropdownMenu(props) {
+  return (
+    <div
+      className={`dropdown-menu ${props.className || ''} ${props.active ? 'show' : ''}`}
+      aria-labelledby={props.id}
+      key="DropdownMenuKey"
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export function DropdownDevider(props) {
+  return <div className="dropdown-divider" />
+}
