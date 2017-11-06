3d40238ab60faec41aae143fcf97c384db298ada
Vadasz Laszlo <maxinteger@gmail.com>
2017-11-06 18:51:10 +0000

    preFetchDB test inkativálása

--- test/functions/middlewares/preFetchDb.spec.js
+++ test/functions/middlewares/preFetchDb.spec.js
@@ -1,7 +1,7 @@
-jest.mock('firebase-functions', () => require('../../../functions/mocks/firebase-functions'))
-jest.mock('firebase-admin', () => require('../../../functions/mocks/firebase-admin').default())
+// jest.mock('firebase-functions', () => require('../../../functions/mocks/firebase-functions'))
+// jest.mock('firebase-admin', () => require('../../../functions/mocks/firebase-admin').default())
 
-const preFetchDB = require('../../../functions/src/middlewares/preFetchDB').default
+// const preFetchDB = require('../../../functions/src/middlewares/preFetchDB').default
 
 describe('preFetchDB', () => {
   it.only('should work', () => {
