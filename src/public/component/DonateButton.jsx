b11295430586fcfcb456342235a5a760881d1fce
Zsebtanár <zsebtanar@users.noreply.github.com>
2018-05-07 20:31:58 +0200

    Revert "Update DonateButton.jsx"

--- src/public/component/DonateButton.jsx
+++ src/public/component/DonateButton.jsx
@@ -8,7 +8,7 @@ export default () => {
         <input type="hidden" name="cmd" value="_s-xclick" />
         <input type="hidden" name="hosted_button_id" value="Y389SASDEHF9A" />
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-        <img alt="" border="0" src="https://www.paypalobjects.com/hu_HU/i/scr/pixel.gif" width="1" height="1" />
+        <img alt="Támogatás Paypal-en keresztül" border="0" src="https://www.paypalobjects.com/hu_HU/i/scr/pixel.gif" width="1" height="1" />
       </form>
     </div>)
 }
