4d6667d571d794d7c6db202726e53ae99ddbfc0e
bojtib <bojtib8@gmail.com>
2020-04-04 21:10:23 +0200

    event handler folytatasa

--- src/server/trigger/userSolvedAnExercise.ts
+++ src/server/trigger/userSolvedAnExercise.ts
@@ -1,8 +1,30 @@
-import { admin } from 'server/utils/firebase'
+import { admin } from '../utils/firebase'
 
 export const userSolvedAnExercise = (snapshot, context) => {
   const userUId = context.params.userUId
   const exerciseId = context.params.exerciseId
-  
-  const possible_rewards = admin.database().ref(`/exercise/${exerciseId}/rewards`)
+  const db = admin.database()
+
+  console.info(`Exercise Id: ${exerciseId}`)
+  db.ref(`/exercise/public/${exerciseId}/rewards`)
+    .once('value')
+    .then(rewardsSnapshot => {
+      const possibleRewards = rewardsSnapshot.val();
+      console.info(possibleRewards)
+
+      if (Array.isArray(possibleRewards)) {
+        console.info(`User UId: ${userUId}`)
+        db.ref(`/userStatistics/${userUId}/solvedExercises`)
+        .once('value')
+        .then(solvedSnapshot => {
+          const solvedExercises = solvedSnapshot.val()
+
+          console.info(solvedExercises)
+
+          possibleRewards.forEach(rewardId => {
+            db.ref(`reward/${rewardId}/additionalInfo/shouldComplete`)
+          });
+        })
+      }
+    })
 }
