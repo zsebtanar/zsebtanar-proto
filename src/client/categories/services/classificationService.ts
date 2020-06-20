6edd6522ef36f034297ea1655fbf5039db4f1c69
Vadasz Laszlo <maxinteger@gmail.com>
2020-06-20 22:05:09 +0100

    small fixes

--- src/client/categories/services/classificationService.ts
+++ src/client/categories/services/classificationService.ts
@@ -3,7 +3,9 @@ import { ClassificationModel } from '../type'
 import { useFetchData } from '../../generic/hooks'
 import { useCallback } from 'react'
 
-const classificationService = new Service<ClassificationModel>('category', { excludeId: true })
+export const classificationService = new Service<ClassificationModel>('category', {
+  excludeId: true
+})
 
 export function useLoadClassifications() {
   const callback = useCallback(() => classificationService.get('classifications'), [])
