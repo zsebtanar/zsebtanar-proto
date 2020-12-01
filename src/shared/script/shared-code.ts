6dbede8c51e197b04dac2a7f01e1735c20b75886
vektor8891 <viktor.szabo123@gmail.com>
2020-12-01 01:13:02 -0500

    feature: new pocketlisp functions (rand-between, divisors, range-between)

--- src/shared/script/shared-code.ts
+++ src/shared/script/shared-code.ts
@@ -1,6 +1,8 @@
 import { literals, PLNumber } from 'pocket-lisp-stdlib'
 import { PseudoRandomNumberGenerator } from '../math/random'
+import { assertInteger, assert } from './shared-code/utils'
 import { range } from '../utils/fn'
+import { listDivisors, randomIntBetween, rangeBetween } from '../utils/math'
 import { langUtils } from './shared-code/langUtils'
 import { stringUtils } from './shared-code/stringUtils'
 
@@ -11,6 +13,20 @@ export const valueSet = (prng: PseudoRandomNumberGenerator) => ({
    */
   ['range']: (x: PLNumber) =>
     literals.Vector.factory(...range(-1, x.value - 1, 1).map((v, k) => literals.Int.factory(k))),
+
+  /**
+   * Generate list of numbers within range
+   *  
+   * @param start first element
+   * @param end   last element
+   */
+  ['range-between']: (start: PLNumber, end: PLNumber) => {
+    assertInteger(start.value)
+    assertInteger(end.value)
+    const rng = rangeBetween(start.value, end.value);
+    return literals.Vector.factory(...rng.map((v, k) => literals.Int.factory(k)))
+  },
+
 
   /**
    * Constant function
@@ -63,6 +79,32 @@ export const valueSet = (prng: PseudoRandomNumberGenerator) => ({
       randInt += randInt % step.value
     }
     return literals.Int.factory(min.value + randInt)
+  },
+  
+  /**
+   * Returns random number in a given range
+   *
+   * @param min   minimum random number
+   * @param max   upper bound
+   */
+  ['rand-between']: (min: PLNumber, max: PLNumber) => {
+    assertInteger(min.value)
+    assertInteger(max.value)
+    const rnd = randomIntBetween(min.value, max.value)
+    return literals.Int.factory(rnd)
+  },
+
+  /**
+   * Returns list of divisors of a number
+   *
+   * @param n number
+   */
+  ['divisors']: (n: PLNumber) => {
+    assertInteger(n.value)
+    assert(n.value < 1, `Number out of range (< 1): ${n.value}`)
+    assert(n.value >= 1000000, `Number too large`)
+		const divisorList = listDivisors(n.value)
+    return literals.Vector.factory(...divisorList.map((v, k) => literals.Int.factory(v)))
   },
   ...langUtils,
   ...stringUtils,
--- src/shared/utils/math.spec.ts
+++ src/shared/utils/math.spec.ts
@@ -1,4 +1,4 @@
-import { floatEq } from './math'
+import { floatEq, listDivisors } from './math'
 
 test('floatEq', () => {
   expect(floatEq(2, 3)).toBe(false)
@@ -8,4 +8,14 @@ test('floatEq', () => {
   expect(floatEq(1 + Number.EPSILON * 2, 1)).toBe(false)
   expect(floatEq(NaN, 1)).toBe(false)
   expect(floatEq(NaN, NaN)).toBe(false)
+})
+
+test('listDivisors', () => {
+  expect(() => {listDivisors(0)}).toThrow('Number out of range (< 1)')
+  expect(() => {listDivisors(1000001)}).toThrow('Number too large')
+  expect(listDivisors(1)).toStrictEqual([1])
+  expect(listDivisors(2)).toStrictEqual([1, 2])
+  expect(listDivisors(4)).toStrictEqual([1, 2, 4])
+  expect(listDivisors(6)).toStrictEqual([1, 2, 3, 6])
+  expect(listDivisors(12)).toStrictEqual([1, 2, 3, 4, 6, 12])
 })
