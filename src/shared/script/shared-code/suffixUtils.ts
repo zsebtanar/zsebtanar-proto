ab86af4172872ea38f57c7df5c1b772b8353f18a
vektor8891 <viktor.szabo123@gmail.com>
2021-01-28 23:04:42 -0500

    refactoring based on code reviewer comments

--- src/shared/script/shared-code/suffixUtils.ts
+++ src/shared/script/shared-code/suffixUtils.ts
@@ -20,8 +20,10 @@ export function getLastNonZeroDigit(num: number): number {
   }
 }
 
+/**
+ * Number of trailing zeros after first non-zero digit (e.g. 230000 -> 4)
+ */
 export function trailingZeros(num: number): number {
-  // number of trailing zeros after first digit
   assertInteger(num)
   if (Math.abs(num) < 10) {
     return 0
@@ -34,27 +36,35 @@ export function trailingZeros(num: number): number {
   return nZeros
 }
 
+/**
+ * Check if big number has even number groups
+ * (see: https://hu.wikipedia.org/wiki/T%C3%ADz_hatv%C3%A1nyai)
+ */
 export function isNumberGroupEven(n: number): boolean {
-  // big numbers (see: https://hu.wikipedia.org/wiki/T%C3%ADz_hatv%C3%A1nyai)
   const nZeros = trailingZeros(n)
   const nGroups = Math.floor(nZeros / 3)
   return nGroups % 2 === 0
 }
 
+/**
+ * Check if number is high pitch (e/i) or low pitch (a/o/u)
+ */
 export function pitch(n: number): string {
-  // check if number is high pitch (e/i) or low pitch (a/o/u)
   assertInteger(n)
   const nZeros = trailingZeros(n)
   if (nZeros <= 1) {
     const highPitch = nZeros === 0 ? [1, 2, 4, 5, 7, 9] : [1, 4, 5, 7, 9]
     const lastDigit = getLastNonZeroDigit(n)
-    return highPitch.indexOf(lastDigit) >= 0 ? 'high' : 'low'
+    return highPitch.includes(lastDigit) ? 'high' : 'low'
   } else if (nZeros >= 3 && nZeros <= 5) {
     return 'high'
   }
   return 'low'
 }
 
+/**
+ * Get vowel for given suffix
+ */
 export function suffixVowel(n: number, vowelType: string): string {
   const lowPitch = pitch(n) === 'low'
   let vowel = ''
@@ -78,8 +88,10 @@ export function suffixVowel(n: number, vowelType: string): string {
   return vowel
 }
 
+/**
+ * Dativ suffix (at/et/öt/t)
+ */
 export function dativSuffix(n: number): string {
-  // suffix: at/et/öt/t
   const nZeros = trailingZeros(n)
   if (nZeros < 6) {
     const lastDigit = getLastDigit(n)
@@ -102,8 +114,10 @@ export function dativSuffix(n: number): string {
   }
 }
 
+/**
+ * Place value suffix (as/es/asoknak/eseket)
+ */
 export function placeValueSuffix(n: number, sfx: string): string {
-  // suffix: as/es/asoknak/eseket etc.
   if (!/^[aáeoö]?s/g.test(sfx)) {
     throw new Error(`Invalid suffix: "${sfx}"`)
   }
@@ -126,16 +140,19 @@ export function placeValueSuffix(n: number, sfx: string): string {
   } else {
     replacement = isNumberGroupEven(n) ? '' : 'o'
   }
-  sfx = sfx.replace(/^[aáeoö]?(?=s)/g, replacement) // -s/as/es/os/ös
-  sfx = sfx.replace(/(?<=s)[eoö](?=k)/g, suffixVowel(n, 'oeö')) // -sok/sek/sök
-  sfx = sfx.replace(/(?<=k)[ae](?=t|l)/g, suffixVowel(n, 'ae')) // -kat/ket/kal/kel
+  sfx = sfx.replace(/^[aáeoö]?s/g, `${replacement}s`) // -s/as/es/os/ös
+  sfx = sfx.replace(/s[eoö]k/g, `s${suffixVowel(n, 'oeö')}k`) // -sok/sek/sök
+  sfx = sfx.replace(/k[ae]t/g, `k${suffixVowel(n, 'ae')}t`) // -kat/ket
+  sfx = sfx.replace(/k[ae]l/g, `k${suffixVowel(n, 'ae')}l`) // -kal/kel
   // replace trailing 2 with 1 to avoid special case: 2-höz vs. 2-esekhez
   sfx = generalSuffix(lastDigit === 2 ? n - 1 : n, sfx)
   return sfx
 }
 
+/**
+ * With suffix (val/vel)
+ */
 export function withSuffix(n: number): string {
-  // suffix: val/vel
   const nZeros = trailingZeros(n)
   if (nZeros <= 1) {
     const lastDigit = getLastNonZeroDigit(n)
@@ -174,16 +191,17 @@ export function withSuffix(n: number): string {
   }
 }
 
+/**
+ * Multiplication suffix (szor/szer/ször)
+ */
 export function multSuffix(n: number, sfx: string): string {
-  // suffix: szor/szer/ször
-
   // replace trailing 2 with 4 to avoid special case: 2-höz vs. 2-szer
   const lastDigit = getLastDigit(n)
   n = lastDigit === 2 ? Math.abs(n) + 2 : n
-  sfx = sfx.replace(/(?<=sz)[oeö](?=r)/g, suffixVowel(n, 'oeö')) // -szor/szer/ször
-  sfx = sfx.replace(/(?<=r)[oeö](?=s)/g, suffixVowel(n, 'oeö')) // -os/es/ös
-  sfx = sfx.replace(/(?<=s)[ae]$/g, suffixVowel(n, 'ae')) // -sa/se
-  sfx = sfx.replace(/(?<=s)[áé]/g, suffixVowel(n, 'áé')) // -sá/sé
+  sfx = sfx.replace(/sz[oeö]r/g, `sz${suffixVowel(n, 'oeö')}r`) // -szor/szer/ször
+  sfx = sfx.replace(/r[oeö]s/g, `r${suffixVowel(n, 'oeö')}s`) // -os/es/ös
+  sfx = sfx.replace(/s[ae]$/g, `s${suffixVowel(n, 'ae')}`) // -sa/se
+  sfx = sfx.replace(/s[áé]/g, `s${suffixVowel(n, 'áé')}`) // -sá/sé
 
   // replace trailing 5 with 7 to avoid special case: 5-höz vs. 5-szöröséhez
   n = lastDigit === 5 ? Math.abs(n) + 2 : n
@@ -191,8 +209,10 @@ export function multSuffix(n: number, sfx: string): string {
   return sfx
 }
 
+/**
+ * Fraction suffix (ad/ed)
+ */
 export function fractionSuffix(n: number, sfx: string): string {
-  // suffix: ad/ed
   const lastDigit = getLastDigit(n)
   const nZeros = trailingZeros(n)
 
@@ -207,28 +227,31 @@ export function fractionSuffix(n: number, sfx: string): string {
   } else {
     replacement = isNumberGroupEven(n) ? 'mo' : 'o'
   }
-  sfx = sfx.replace(/^[aemoö]?(?=d)/g, replacement) // -d/ad/ed/od/öd/mod
-  sfx = sfx.replace(/(?<=d)[ae]$/g, suffixVowel(n, 'ae')) // -da/de
-  sfx = sfx.replace(/(?<=d)[áé]/g, suffixVowel(n, 'áé')) // -dá/dé
-  sfx = sfx.replace(/(?<=[dv])[ae](?=l)/g, suffixVowel(n, 'ae')) // -dal/del/val/vel
+  sfx = sfx.replace(/^[aemoö]?d/g, `${replacement}d`) // -d/ad/ed/od/öd/mod
+  sfx = sfx.replace(/d[ae]$/g, `d${suffixVowel(n, 'ae')}`) // -da/de
+  sfx = sfx.replace(/d[áé]/g, `d${suffixVowel(n, 'áé')}`) // -dá/dé
+  sfx = sfx.replace(/d[ae]l/g, `d${suffixVowel(n, 'ae')}l`) // -dal/del
+  sfx = sfx.replace(/v[ae]l/g, `v${suffixVowel(n, 'ae')}l`) // -val/vel
 
   sfx = multSuffix(n, sfx) // -adszorosa/edszerese
   return sfx
 }
 
+/**
+ * General suffixes (nak/nek, ból/ből, hoz/hez/höz)
+ */
 export function generalSuffix(n: number, sfx: string): string {
-  // suffix: nak/nek, ból/ből, hoz/hez/höz
-  sfx = sfx.replace(/(?<=b)[óő](?=l)/g, suffixVowel(n, 'óő')) // -ból/ből
-  sfx = sfx.replace(/(?<=n)[ae](?=k)/g, suffixVowel(n, 'ae')) // -nak/nek
-  sfx = sfx.replace(/(?<=h)[eoö](?=z)/g, suffixVowel(n, 'oeö')) // -hoz/hez/höz
-  sfx = sfx.replace(/(?<=r)[ae]$/g, suffixVowel(n, 'ae')) // -ra/re
+  sfx = sfx.replace(/b[óő]l/g, `b${suffixVowel(n, 'óő')}l`) // -ból/ből
+  sfx = sfx.replace(/n[ae]k/g, `n${suffixVowel(n, 'ae')}k`) // -nak/nek
+  sfx = sfx.replace(/h[eoö]z/g, `h${suffixVowel(n, 'oeö')}z`) // -hoz/hez/höz
+  sfx = sfx.replace(/r[ae]$/g, `r${suffixVowel(n, 'ae')}`) // -ra/re
   return sfx
 }
 
 export function convertSuffix(n: number, sfx: string): string {
   if (/^[aáeoöő]?t$/g.test(sfx)) {
     return dativSuffix(n)
-  } else if (/^[aáeoö]?s(?!z)/g.test(sfx)) {
+  } else if (/^[aáeoö]?s$/g.test(sfx) || /^[aáeoö]?s[^z]/g.test(sfx)) {
     return placeValueSuffix(n, sfx)
   } else if (/^[acdegmnrstvyz]{2,}l$/g.test(sfx)) {
     return withSuffix(n)
