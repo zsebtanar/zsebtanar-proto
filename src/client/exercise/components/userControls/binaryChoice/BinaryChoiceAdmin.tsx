041aae76928cec63480d737eb118c30ccb42a9ac
vektor8891 <viktor.szabo123@gmail.com>
2021-02-24 00:59:23 -0500

    WIP: enabling dynamic solution for binary choice

--- src/client/exercise/components/userControls/binaryChoice/BinaryChoiceAdmin.tsx
+++ src/client/exercise/components/userControls/binaryChoice/BinaryChoiceAdmin.tsx
@@ -10,17 +10,27 @@ import { MarkdownWithScript } from 'client/script/components/MarkdownWithCode'
 import { Select } from 'client/generic/components/form/input/Select'
 import { Icon } from 'client/generic/components/icons/Icon'
 import { Alert } from '../../../../generic/components/Alert'
+import { usePocketLisp } from 'client/script/providers/PocketLispProvider'
+import { PLString, PLVector } from 'pocket-lisp-stdlib'
 
 export function BinaryChoiceAdmin(bindProps: UseModelProps<UCBinaryChoice>): JSX.Element {
   const { bind, data, append, remove } = useModel<UCBinaryChoice>(bindProps)
+  const { evalPL } = usePocketLisp()
+  let solution: string[][] = []
+  let hasSolution = false
+  if (data.isDynamic) {
+    const dynamicSolution = evalPL(`(solution-${data.name})`) as PLVector<PLVector<PLString>>
+    if (dynamicSolution !== undefined) {
+      solution = dynamicSolution.value.map((x) => x.value.map((y) => y.toString()))
+      hasSolution = true
+    }
+  }
 
   return (
     <div className="user-control uc-binary-choice uc-binary-choice-admin">
       <UserControlNameInput {...bind('name')} />
       <div>
-        <Checkbox {...bind('isDynamic')} disabled>
-          Dinamikus
-        </Checkbox>
+        <Checkbox {...bind('isDynamic')}>Dinamikus</Checkbox>
       </div>
 
       <hr />
@@ -33,88 +43,90 @@ export function BinaryChoiceAdmin(bindProps: UseModelProps<UCBinaryChoice>): JSX
 
       <hr />
 
-      <h6>
-        {' '}
-        Megoldások{' '}
-        <Button
-          btn="link"
-          small
-          onAction={() => {
-            append('props.options', '')
-            append('solution', '')
-          }}
-        >
-          <PlusCircle /> Alternatív megoldás megadása
-        </Button>
-      </h6>
-      {!data.props?.options?.length && (
-        <Alert type="warning">Nincs megadva választási lehetőség</Alert>
-      )}
-      {!!data.props?.options?.length && (
-        <table className="table table-borderless table-sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th className="w-25">Leírás</th>
-              <th className="w-25">Igaz állítás</th>
-              <th className="w-25">Hamis állítás</th>
-              <th>Megoldás</th>
-              <th></th>
-            </tr>
-          </thead>
+      <h6>Megoldások</h6>
+      {!!data.isDynamic && <div></div>}
+      {!data.isDynamic && (
+        <div>
+          <Button
+            btn="link"
+            small
+            onAction={() => {
+              append('props.options', '')
+              append('solution', '')
+            }}
+          >
+            <PlusCircle /> Alternatív megoldás megadása
+          </Button>
+          {!data.props?.options?.length && (
+            <Alert type="warning">Nincs megadva választási lehetőség</Alert>
+          )}
+          {!!data.props?.options?.length && (
+            <table className="table table-borderless table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th className="w-25">Leírás</th>
+                  <th className="w-25">Igaz állítás</th>
+                  <th className="w-25">Hamis állítás</th>
+                  <th>Megoldás</th>
+                  <th></th>
+                </tr>
+              </thead>
 
-          <tbody>
-            {data.props?.options?.map((item, idx) => (
-              <tr key={idx}>
-                <td>{idx + 1}</td>
-                <td>
-                  <TextEditor
-                    {...bind(`props.options.${idx}.statement`)}
-                    preview={MarkdownWithScript}
-                    inline
-                  />
-                </td>
-                <td>
-                  <TextEditor
-                    {...bind(`props.options.${idx}.trueLabel`)}
-                    preview={MarkdownWithScript}
-                    inline
-                  />
-                </td>
-                <td>
-                  <TextEditor
-                    {...bind(`props.options.${idx}.falseLabel`)}
-                    preview={MarkdownWithScript}
-                    inline
-                  />
-                </td>
-                <td>
-                  <Select
-                    className="form-control"
-                    options={[
-                      { label: item.trueLabel, value: true },
-                      { label: item.falseLabel, value: false },
-                    ]}
-                    {...bind(`solution.${idx}`)}
-                  />
-                </td>
-                <td>
-                  <Button
-                    small
-                    btn="link"
-                    className="text-danger"
-                    onAction={() => {
-                      remove(`props.options.${idx}`)
-                      remove(`solution.${idx}`)
-                    }}
-                  >
-                    <Icon icon={TrashIcon} />
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              <tbody>
+                {data.props?.options?.map((item, idx) => (
+                  <tr key={idx}>
+                    <td>{idx + 1}</td>
+                    <td>
+                      <TextEditor
+                        {...bind(`props.options.${idx}.statement`)}
+                        preview={MarkdownWithScript}
+                        inline
+                      />
+                    </td>
+                    <td>
+                      <TextEditor
+                        {...bind(`props.options.${idx}.trueLabel`)}
+                        preview={MarkdownWithScript}
+                        inline
+                      />
+                    </td>
+                    <td>
+                      <TextEditor
+                        {...bind(`props.options.${idx}.falseLabel`)}
+                        preview={MarkdownWithScript}
+                        inline
+                      />
+                    </td>
+                    <td>
+                      <Select
+                        className="form-control"
+                        options={[
+                          { label: item.trueLabel, value: true },
+                          { label: item.falseLabel, value: false },
+                        ]}
+                        {...bind(`solution.${idx}`)}
+                      />
+                    </td>
+                    <td>
+                      <Button
+                        small
+                        btn="link"
+                        className="text-danger"
+                        onAction={() => {
+                          remove(`props.options.${idx}`)
+                          remove(`solution.${idx}`)
+                        }}
+                      >
+                        <Icon icon={TrashIcon} />
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
       )}
     </div>
   )
