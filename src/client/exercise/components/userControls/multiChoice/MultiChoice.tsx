d278a2487d0f7d1ef659aed7ab038e050d643ebd
Vadasz Laszlo <maxinteger@gmail.com>
2021-04-13 23:37:04 +0200

    fix: multi and binary choice style and add default labels
    issue: #590

--- src/client/exercise/components/userControls/multiChoice/MultiChoice.tsx
+++ src/client/exercise/components/userControls/multiChoice/MultiChoice.tsx
@@ -5,6 +5,8 @@ import { Checkbox } from 'client/generic/components/form/input/Checkbox'
 import { MarkdownWithScript } from 'client/script/components/MarkdownWithCode'
 import { filledArray } from 'shared/utils/fn'
 
+import './MultiChoice.scss'
+
 interface Props extends UseModelProps<boolean[]> {
   ctrl: UCMultiChoice
   readonly?: boolean
@@ -22,12 +24,12 @@ export function MultiChoice({ readonly, ctrl, disabled, ...bindProps }: Props): 
         ? ctrl?.props?.options
             .filter((_, idx) => ctrl?.solution[idx])
             .map(({ label }, idx) => (
-              <div key={idx}>
+              <div key={idx} className="uc-multi-choice-item">
                 <MarkdownWithScript source={label} />
               </div>
             ))
         : ctrl?.props?.options.map(({ label }, idx) => (
-            <div key={idx}>
+            <div key={idx} className="uc-multi-choice-item">
               <Checkbox {...bind(`${idx}`)} disabled={disabled}>
                 <MarkdownWithScript source={label} />
               </Checkbox>
